70ebd057d2ea8da390118a36bd5f819ff70c2620
J <61575600+joncode1@users.noreply.github.com>
2020-08-20 23:35:35 -0700

    v1.3
    Added 'isomorphic-unfetch' for fetch using NPM

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,26 @@
+import fetch from 'isomorphic-unfetch'
 import Head from 'next/head'
 import styles from "../styles/index.module.css";
 
-const AboutPage = () =>(
+const AboutPage = (myWord) =>(
     <div className={styles.container}>
         <Head>
-            <title>About | Dictionary </title>
+            <title>About  | Dictionary </title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <h1>About Page</h1>
+        <p>Updated on: {myWord.thePassed.price}</p>
     </div>
 );
+
+AboutPage.getInitialProps = async function() {
+    const myRES = await fetch('https://api.binance.us/api/v3/ticker/price?symbol=ETHUSD');
+    const myDATA = await myRES.json();
+    return {
+        thePassed: myDATA,
+
+    };
+}
 
 export default AboutPage;
