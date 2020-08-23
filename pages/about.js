31ee0f4215f28ad5dae65a2986f1eed9d4ad07f4
J <61575600+joncode1@users.noreply.github.com>
2020-08-23 01:51:16 -0700

    v1.7
    Removed Axios
    Used fetch from isomorphic-unfetch
    Hide API keys in env

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,8 @@
 import fetch from 'isomorphic-unfetch'
-import axios from 'axios'
 import Head from 'next/head'
 import styles from "../styles/index.module.css";
 import process from "../next.config"
 
-const api = axios.create({
-
-    "method":"GET",
-    "url":"https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf",
-    "headers":{
-        "content-type":"application/octet-stream",
-        "x-rapidapi-host":"wordsapiv1.p.rapidapi.com",
-        "x-rapidapi-key":"REPLACE",
-        "useQueryString":true
-    }
-})
-console.log(process.env.test)
 const AboutPage = (myWord) =>(
 
     <div className={styles.container}>
@@ -25,20 +12,29 @@ const AboutPage = (myWord) =>(
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <h1>About Page</h1>
-        <p>Updated on: {myWord.thePassed.price}</p>
+        <h1>About Page2</h1>
+        <p>Updated on: {myWord.returned.word}</p>
     </div>
 
 );
 
-AboutPage.getInitialProps = async function() {
 
-    const myRES = await fetch('https://api.binance.us/api/v3/ticker/price?symbol=ETHUSD');
+
+
+AboutPage.getInitialProps = async function() {
+    const myRES = await fetch("https://wordsapiv1.p.rapidapi.com/words/computer/typeOf", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+            "x-rapidapi-key": process.env.apiKeyWords,
+        }
+    });
     const myDATA = await myRES.json();
 
     return {
-        thePassed: myDATA,
+        returned: myDATA,
     };
+
 }
 
 export default AboutPage;
